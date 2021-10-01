fc51b0c987f2204bc2e2fed392a41fe7a346afaf
Kyle M. Hart <kylemhart@gmail.com>
2021-10-01 10:42:01 -0400

    Create shell of function and tests

---
+++ get_user_info.py
@@ -0,0 +1,12 @@
+from typing import Dict
+
+
+def getUserInfo(username: str) -> Dict[str, int]:
+    """!
+    @brief Returns the repositories and number of commits for a given username on GitHub
+
+    @param username The username to lookup on GitHub
+    @return Dict[str, int] A dictionary where the keys are the names of public repositories and values are the number
+    of commits per repository.
+    """
+    return dict()
---
+++ test_get_user_info.py
@@ -0,0 +1,11 @@
+from get_user_info import getUserInfo
+import unittest
+
+
+class TestGetUserInfo(unittest.TestCase):
+    """!
+    @brief A class to test the getUserInfo function.
+    """
+
+    def test_dummy(self):
+        pass
